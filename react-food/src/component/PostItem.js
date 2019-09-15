b23cabe6346cc1a364d6260b220e1d354d60dc05
Yasmin-Alhajjaj <yasmin.alhajjaj@gmail.com>
2019-09-15 11:00:36 +0300

    herkou deploy21

--- react-food/src/component/PostItem.js
+++ react-food/src/component/PostItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import "../App.css";
 
 export default class PostItem extends Component {
  state = {
@@ -36,15 +37,16 @@ update=(bookedup)=>{
          
          return (
            <div key={key} className="col-4" style={{ marginTop:"30px" ,display:"flex" , justifyContent:"space-around"}} >
-           <div className="card" style={{ width: "25rem",border: 'solid 1px black' }}>
+           <div className="card hoverr" style={{ width: "25rem",border: 'solid 1px black',borderRadius:"10px", backgroundColor:"#F5FEFE"
+ }}>
              <img src={post.imageUrl}  className="card-img-top" alt="" style={{ height: "17rem", }}/>
              <div className="card-body" style={{textAlign:"right" }}  >
                <h5 className="card-title">{post.namefood}</h5>
                <p className="card-text"><span style={{color:"gray",fontSize:"13px" }} > {post.description}</span>  </p>
                </div>
                <ul className="list-group list-group-flush">
-               <li className="list-group-item"><span style={{color:"gray"}} >amount:  </span> {post.amount}</li>
-               <li className="list-group-item"> <span style={{color:"gray"}} >location: </span> {post.location}</li>
+               <li className="list-group-item"style={{ backgroundColor:"#F5FEFE"  }} ><span style={{color:"gray"}} >amount:  </span> {post.amount}</li>
+               <li className="list-group-item"style={{ backgroundColor:"#F5FEFE"  }}> <span style={{color:"gray"}} >location: </span> {post.location}</li>
                </ul>
                <div className="card-body" >
               <button className="btn btn-dark" onClick={this.update.bind(this,post._id)}  style={{width:"100%" , color:(post.booking)?"#bae637":"red"  }}  >BOOKING</button>
